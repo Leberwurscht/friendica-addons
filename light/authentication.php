8c7a0239dac31eeaaadb1cebd2df45b4e43d543a
Leberwurscht <leberwurscht@hoegners.de>
2012-07-19 22:26:07 +0200

    light addon: add comments

--- light/authentication.php
+++ light/authentication.php
@@ -1,6 +1,12 @@
 <?php
 
 function light_authenticate() {
+	// - read token from _REQUEST variable
+	// - check if there is an entry in the pconfig table with this token
+	// - from this entry, we get the contact id
+	// - return correspondig contact row in case of success, otherwise return false
+	// - also, check whether user has deactivated the addon in the user settings
+
 	$token = $_REQUEST["token"];
 	$r = q("SELECT * FROM `pconfig` WHERE `cat`='light' AND `k` LIKE 'token:%%' AND `v`='%s'",
 		dbesc(hash('whirlpool', $token))
